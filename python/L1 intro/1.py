b10a61131f4577f5c64010100bd39ea20b0d6a2e
shema1813 <shemarutagwera@gmail.com>
2026-04-09 20:43:57 +0300

    1

---
+++ python/L1 intro/1.py
@@ -0,0 +1,4 @@
+print("Hello world")
+print("My name is Shema")
+print("I love games")
+print("I can play the math for you", 7*5)
---
+++ python/L1 intro/2.py
@@ -0,0 +1,7 @@
+print("Welcome to the world of programming")
+print("5")
+print("I will print to next line \nAs you see I am next line")
+
+import keyword
+print("The import keywords in python:\n")
+print(keyword.kwlist)
