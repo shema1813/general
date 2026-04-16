4f10acd7c6abc966764085e2a2add4224e36365f
shema1813 <shemarutagwera@gmail.com>
2026-04-16 20:39:15 +0300

    1.py

---
+++ python/L3 conditional statements/date time.py
@@ -0,0 +1,12 @@
+import datetime
+
+#using now() to get current time
+current_time = datetime.datetime.now()
+
+#printing value of now.
+print("Time is now at greenwich meridian is:", end="")
+print(current_time)
+
+#print calendar of 2021
+import calendar
+print("\n", calendar.calendar(2021))
