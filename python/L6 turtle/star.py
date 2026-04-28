e22030f64a1de72729d8daa1b6d2628319835408
shema1813 <shemarutagwera@gmail.com>
2026-04-28 18:55:46 +0300

    star.py

---
+++ python/L6 turtle/star.py
@@ -0,0 +1,25 @@
+import turtle
+screen = turtle.Screen()
+screen.bgcolor('Aqua')
+
+board = turtle.Turtle()
+
+board.forward(100)
+board.left(120)
+board.forward(100)
+board.left(120)
+board.forward(100)
+
+board.penup()
+board.right(150)
+board.forward(50)
+board.pendown()
+
+board.right(90)
+board.forward(100)
+board.right(120)
+board.forward(100)
+board.right(120)
+board.forward(100)
+
+turtle.done()
