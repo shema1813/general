d53a44ccbe6de7b62c09a96cfe3088ee2d1e09b1
shema1813 <shemarutagwera@gmail.com>
2026-09-03 20:43:28 +0300

    1.py

---
+++ python/L31 tables/2.sql
@@ -0,0 +1,37 @@
+create TABLE IF NOT EXISTS Salesman (
+    Salesman_id TEXT PRIMARY KEY,
+    NAME TEXT,
+    comission REAL,
+    CITY TEXT
+);
+
+INSERT INTO Salesman (Salesman_id, NAME, comission, CITY)VALUES
+('5001', 'James Hoog', 'New York', 0.15),
+('5002', 'Nail Knite', 'Paris', 0.13),
+('5005', 'Pit Alex', 'London', 0.11),
+('5006', 'Mc Lyon', 'Paris', 0.14),
+('5007', 'Paul Adam', 'Rome', 0.13),
+('5003', 'Lauson Hen', 'San Jose', 0.12);
+
+SELECT * FROM Salesman;
+
+Create TABLE IF NOT EXISTS Orders (
+ord_no TEXT primary KEY,
+purch_amt REAL,
+ord_date TEXT,
+customer_id TEXT,
+Salesman_id TEXT
+);
+
+INSERT INTO ORDERS (ord_no, purch_amt, ord_date, customer_id, salesman_id)VALUES
+('70001', 150.5, '2012-10-05', '3005', '5002'),
+('70009', 270.65, '2012-09-10', '3001', '5001'),
+('70002', 65.26, '2012-10-05', '3002', '5003'),
+('70004', 110.5, '2012-08-17', '3009', '5007'),
+('70007', 948.5, '2012-09-10', '3005', '5005'),
+('70005', 2400.6, '2012-07-27', '3007', '5006');
+
+SELECT * FROM ORDERS;
+
+SELECT NAME, Comission
+FROM Salesman;
